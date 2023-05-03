b2bcf2a838eeeaffde25a7f03ea2a77bd8889d8e
WAUT6 <yuri.911@hotmail.com>
2023-05-03 20:11:20 +0300

    step-14

--- lib/views/home_menu_view.dart
+++ lib/views/home_menu_view.dart
@@ -1,3 +1,4 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:metapp/bloc/chat_bloc/chat_bloc.dart';
@@ -45,6 +46,27 @@ class _HomeMenuViewState extends State<HomeMenuView> {
       builder: (context, state) {
         if (state is ViewStateViewingHomePage) {
           return Scaffold(
+            bottomNavigationBar: ConvexAppBar(
+              items: const [
+                TabItem(
+                  icon: Icons.book,
+                  title: 'Items',
+                ),
+                TabItem(
+                  icon: Icons.shopping_cart,
+                  title: 'Orders',
+                ),
+                TabItem(
+                  icon: Icons.message,
+                  title: 'Messages',
+                ),
+                TabItem(
+                  icon: Icons.settings,
+                  title: 'Settings',
+                ),
+              ],
+              onTap: (index) {},
+            ),
             appBar: AppBar(
               title: const Text('MET APP'),
               centerTitle: true,
